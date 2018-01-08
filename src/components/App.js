7ec8b3fd770e9d8580c1c3b419aae8fda0ab5d26
SherylHohman <SherylHohman@users.noreply.github.com>
2018-01-07 20:09:22 -0800

    feat: add basic UI for App (input box and message)

--- src/components/App.js
+++ src/components/App.js
@@ -20,14 +20,17 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input
+          type='text'
+          ref={(input) => this.input = input}
+          placeholder="Monday's Breakfast"
+        />
+        <button onClick={this.submitFood}>Submit</button>
+
+        <pre>
+          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
+        </pre>
       </div>
     );
   }
