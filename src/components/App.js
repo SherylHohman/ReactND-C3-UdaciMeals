90f0b442f4199b98c4ae1832ebbfff28437f8211
SherylHohman <SherylHohman@users.noreply.github.com>
2018-01-12 05:47:05 -0800

    feat: react-redux Provider,connect -> dispatch in App.js
    - in index.js, Wrap the App component with the Provider component from
    	react-redux, and pass it the redux `store` as a prop.
    - in App.js. use react-redex to `export default connect()(App);`
    	binding store.dispatch to the App component (curried).
    This enables the redux store's `dispatch` function to be available in
    	App.js as `this.props.dispatch`.
    
    TLDR: to `dispatch` an action in a component, simply `connect` the
    	the component to the Provider.
    
    TODO: we actually want the store's variables (state) to be available
    	to App.js.  The dispatch method alone is not sufficient to
    	accomplish our goal with App.js.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from '../logo.svg';
 import '../App.css';
 
 class App extends Component {
   render() {
+    console.log('Props:', this.props);
     return (
       <div className="App">
         <header className="App-header">
@@ -16,4 +18,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
