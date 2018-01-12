44d3328a602db61f2e32bc62b4561ae7de8f6936
SherylHohman <SherylHohman@users.noreply.github.com>
2018-01-11 16:17:01 -0800

    feat: revert App to default create-react-app render
    why: we're about to refactor the whole app
    	using "connect" from react-redux.  The previous render was just
    	a preliminary "learning" experience.
    "connect" will connect components to the data store in an entirely
    	different way, so references, and hence rendering code, will be
    	quite different. Best to start with a clean slate.

--- src/components/App.js
+++ src/components/App.js
@@ -1,63 +1,19 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
-import { addRecipe } from '../actions/index.js';
 
 class App extends Component {
-  state = {
-    calendar: null
-  }
-
-  componentDidMount(){
-    const { store } = this.props;
-
-    // anytime store is modified, call setState to update the calendar `state` for this component
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-  }
-
-  submitFood = () => {
-    // note form of function above ! not: submitFood() {...} method. UGH.
-
-    this.props.store.dispatch(addRecipe(
-      {
-        day: 'monday',
-        meal: 'breakfast',
-        recipe: {
-          label: this.input.value
-        }
-      }
-    ));
-
-    // clear input form
-    this.input.value = '';
-  }
-
-  render(){
+  render() {
     return (
-      <div>
-        <input
-          type='text'
-          {/* this looks like an UNcontrolled input form to me..*/}
-          {/*  https://reactjs.org/docs/uncontrolled-components.html*/}
-          ref={(input) => this.input = input}
-          placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-
-        <pre>
-          Monday's Breakfast: {this.state.calendar &&
-                               this.state.calendar.monday.breakfast
-                              }
-        </pre>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React-Redux</h1>
+        </header>
+        <p className="App-intro"></p>
       </div>
     );
   }
-
-
 }
 
 export default App;
