bf9289a0d5c232a1616ef6afa371dee44818b66a
SherylHohman <SherylHohman@users.noreply.github.com>
2018-01-13 15:37:18 -0800

    feat: write a `food` reducer to store recipe details

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,26 @@ import {
   REMOVE_FROM_CALENDAR
 } from '../actions';
 
+function food(state = {}, action) {
+  switch (action.type){
+    case ADD_RECIPE:
+      const { recipe } = action;
+      return ({
+        ...state,
+
+        [recipe.label]: action.recipe,  // ..so I can mentally "see" what I'm accessing
+        // [recipe.label]: recipe,     // Udacidy codes it this way - it's cleaner!
+
+        // recipe.label is "pizza" - string typed in by user to label the recipe.  Hence it *must* be destructured from action as above.
+        //  It's not a property name in itself, it's a variable holding
+        //  the string name of the property to be added.
+
+      });
+    default:
+      return state;
+  }
+}
+
 const initialCalendarState = {
     sunday: {
       breakfast: null,
