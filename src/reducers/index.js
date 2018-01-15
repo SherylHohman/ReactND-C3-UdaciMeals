a956f58603e9d15aa0dcfc3c321724c98a8fdaa6
SherylHohman <SherylHohman@users.noreply.github.com>
2018-01-15 00:00:58 -0800

    feat: add sample food menu and calendar data ! :-)
    For Real this time!

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,20 +5,52 @@ import {
 } from '../actions';
 
 const sampleMenu = {
-  "pizza": "crust, cheese, toppings",
-  "movie popcorn": "popcorn, butter, oil, salt",
-  "mimosa": "OJ, alcohol"
+  // leftover Pizza isn't actually in Edamam..
+  "Leftover Pizza": {
+    label: "Leftover Pizza",
+    image: "https://www.edamam.com/web-img/bc8/bc803d882221f2c29d280bd9f6155ab0.jpg",
+    ingredientLines: [
+      "cheese",
+      "crust",
+      "toppings",
+    ]
+  },
+  "The Bitter Mimosa Recipe": {
+    label: "The Bitter Mimosa Recipe",
+    image: "https://www.edamam.com/web-img/f5d/f5da659cdd28b390f172b34c8ec56092.jpg",
+    ingredientLines: [
+      "1/2 ounce Cynar",
+      "3 ounces freshly squeezed pink grapefruit juice from 1 to 2 grapefruits",
+      "3 ounces chilled sparkling wine"
+    ],
+  },
+
+  "Herbed Buttermilk Popcorn recipes": {
+    label: "Herbed Buttermilk Popcorn recipes",
+    image: "https://www.edamam.com/web-img/852/8527512be888b22a6c73108944d40d44",
+    ingredientLines: [
+      "1 tablespoon powdered buttermilk",
+      "1 teaspoon garlic powder",
+      "1 teaspoon onion powder",
+      "1 teaspoon lemon pepper",
+      "1/2 teaspoon dried dill weed",
+      "1/2 teaspoon powdered chicken bouillon or kosher salt",
+      "1 tablespoon corn oil",
+      "1/3 cup popcorn kernals",
+      "2 tablespoons unsalted butter"
+    ],
+  }
 }
 
 const sampleCalendar = {
   sunday: {
-    breakfast: 'pizza',
-    lunch: 'mimosa',
-    dinner: 'movie popcorn',
+    breakfast: "Leftover Pizza",
+    lunch: "The Bitter Mimosa Recipe",
+    dinner: "Herbed Buttermilk Popcorn recipes",
   },
 }
 
-function food(state = {}, action) {
+function food(state = sampleMenu, action) {
   switch (action.type){
     case ADD_RECIPE:
       const { recipe } = action;
@@ -69,7 +101,7 @@ const initialCalendarState = {
     }
 }
 
-function calendar (state = initialCalendarState, action) {
+function calendar (state = { ...initialCalendarState, ...sampleCalendar }, action) {
   const { day, meal, recipe } = action;
 
   switch(action.type){
@@ -102,7 +134,9 @@ export default combineReducers({
 
 
 // Notes:
-        // recipe.label is "pizza" - string typed in by user to label the recipe.  Hence it *must* be destructured from action as above.
-        //  It's not a property name in itself, it's a variable holding
-        //  the string name of the property to be added.
+    // recipe.label is "pizza" - string typed in by user to label the recipe.
+    //   Hence it *must* be destructured from action as above.
+    //   It's not a property name in itself.
+    //   It's a variable holding
+    //   the string name of the property to be added.
 
